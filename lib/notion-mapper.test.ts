1f411651030d8eda4e173c1b0b57275d2865700c
wataru-ono <wtr0414@gmail.com>
2026-08-22 11:41:08 +0900

    test(notion): use relative mapper import

--- lib/notion-mapper.test.ts
+++ lib/notion-mapper.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from "vitest"
 import {
   mapNotionPageToFitnessLog,
   mapNotionPageToWorkoutSet,
-} from "@/lib/notion-mapper"
+} from "./notion-mapper"
 import { notionMapperPageFixtures } from "./notion-mapper-fixture"
 
 describe("mapNotionPageToFitnessLog", () => {
